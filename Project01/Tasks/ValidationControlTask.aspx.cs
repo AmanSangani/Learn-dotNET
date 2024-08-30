cec69a7efd6661d8e49eb4e6e3732e141c07a45a
AmanSangani <sanganiaman1310@gmail.com>
2024-08-30 23:56:46 +0530

    Form Validation

---
+++ Project01/Tasks/ValidationControlTask.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project01.Tasks
+{
+    public partial class ValidationControl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            lblSaveMsg.Text = "Data Saved Successfully...";
+        }
+    }
+}
