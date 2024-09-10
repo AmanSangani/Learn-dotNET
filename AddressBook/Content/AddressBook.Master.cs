904e4a2094bf65ac1e654e0d4008c2fe5962e412
AmanSangani <sanganiaman1310@gmail.com>
2024-09-10 15:13:29 +0530

    Added Username/Email in Header of User

--- AddressBook/Content/AddressBook.Master.cs
+++ AddressBook/Content/AddressBook.Master.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -14,6 +17,83 @@ namespace AddressBook.Content
             if (Session["UserID"]  == null)
             {
                 Response.Redirect("~/AdminPanel/Auth/Login.aspx");
+            }
+            else
+            {
+                String DisplayName = "";
+                String email = "";
+
+                #region Establish Connection
+
+                SqlConnection connObj = new SqlConnection();
+
+                connObj.ConnectionString = ConfigurationManager.ConnectionStrings["AddressBookConnectionString"].ConnectionString;
+
+                #endregion Establish Connection
+
+                try
+                {
+                    #region Connection and Command Object
+
+                    if (connObj.State != ConnectionState.Open)
+                    {
+                        connObj.Open();
+                    }
+
+                    SqlCommand cmdObj = new SqlCommand();
+
+                    cmdObj.Connection = connObj;
+
+                    cmdObj.CommandType = CommandType.StoredProcedure;
+
+                    #endregion Connection and Command Object
+
+                    #region Store Procedure, Execute and Read/Bind Data
+
+                    cmdObj.CommandText = "PR_User_SelectByPK";
+
+                    cmdObj.Parameters.AddWithValue("@UserID", Session["UserID"].ToString());
+
+                    SqlDataReader sdrObj = cmdObj.ExecuteReader();
+
+                    while(sdrObj.Read())
+                    {
+                        if (!sdrObj["DisplayName"].Equals(DBNull.Value))
+                        {
+                            DisplayName = sdrObj["DisplayName"].ToString();
+                        }
+                        if (!sdrObj["email"].Equals(DBNull.Value))
+                        {
+                            email = sdrObj["email"].ToString();
+                        }
+                        break;
+                    }
+
+                    #endregion Store Procedure, Execute and Read/Bind Data
+                }
+
+                #region Exception Handling
+                catch (SqlException sqlEx)
+                {
+                    Response.Write("SQL Error: " + sqlEx.Message);
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error: " + ex.Message);
+                }
+                #endregion Exception Handling
+
+                #region Close Connection
+                finally
+                {
+                    if (connObj.State == ConnectionState.Open)
+                    {
+                        connObj.Close();
+                    }
+                }
+                #endregion Close Connection
+
+                lblUsernameMsj.Text = "Hello " + DisplayName + "  |  Email: " + email;
             }
         }
 
